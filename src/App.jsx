792efe5d4ec7c399a82752c2492333b4f046526a
Adrian frs <shouasobu@gmail.com>
2024-12-01 14:46:51 +0700

    Fetching nasa API and add a loading state feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,48 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Footer from "./components/Footer"
 import Main from "./components/Main"
 import SideBar from "./components/SideBar"
 
 
 function App() {
-  const [showModal, setShowModal] = useState(false)
+  const [data, setData] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const [showModal, setShowModal] = useState(false);
   
   function handleToggleModal() {
     setShowModal(!showModal)
   }
+  
+  useEffect(() => {
+    async function fetchAPIData() {
+      const NASA_KEY = import.meta.env.VITE_NASA_API_KEY;
+      const url = 'https://api.nasa.gov/planetary/apod' + `?api_key=${NASA_KEY}`
+      try {
+        const res = await fetch(url);
+        const apiData = await res.json()
+        setData(apiData)
+        console.log('DATA\n', apiData)
+      } catch (error) {
+        console.log(error.message);
+        
+      }
+    }
+    fetchAPIData()
+  }, [])
 
   return (
     <>
-      <Main />
+      {data ? (<Main />) : (
+        <div className="loadingState">
+          <i className="fa-solid fa-gear"></i>
+        </div>
+      )}
       {showModal && (
         <SideBar handleToggleModal={handleToggleModal} />
       )}
-      <Footer 
-        handleToggleModal={handleToggleModal}
-      />
+      {data && (
+        <Footer handleToggleModal={handleToggleModal} />
+      )}
     </>
   )
 }
