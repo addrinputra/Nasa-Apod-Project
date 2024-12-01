2e8f5d5320e8eba4e7232524e14feead0fae3937
Adrian frs <shouasobu@gmail.com>
2024-12-01 14:26:55 +0700

    Show Toggle Modal feature

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,8 @@
+import PropTypes from 'prop-types'
 
-export default function Footer() {
+export default function Footer(props) {
+  const { handleToggleModal } = props;
+
   return (
     <footer>
       <div className="bgGradient"></div>
@@ -7,9 +10,13 @@ export default function Footer() {
         <h2>The Brutal Martian Landscape</h2>
         <h1>APOD PROJECT</h1>
       </div>
-      <button>
+      <button onClick={handleToggleModal}>
         <i className="fa-solid fa-circle-info"></i>
       </button>
     </footer>
   )
+}
+
+Footer.propTypes = {
+  handleToggleModal: PropTypes.func.isRequired,
 }
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,18 +1,26 @@
+import PropTypes from 'prop-types'
 
-export default function SideBar() {
+
+export default function SideBar(props) {
+  const {handleToggleModal} = props;
+
   return (
     <div className="sidebar">
-      <div className="bgOverlay"></div>
+      <div onClick={handleToggleModal} className="bgOverlay"></div>
       <div className="sidebarContents">
         <h2>The Brutal Martian Landscape</h2>
         <div>
           <p>Description</p>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde non vitae eos, aut doloribus aperiam tenetur animi rerum neque temporibus?</p>
         </div>
-        <button>
+        <button onClick={handleToggleModal}>
           <i className="fa-solid fa-arrow-right"></i>
         </button>
       </div>
     </div>
   )
+}
+
+SideBar.propTypes = {
+  handleToggleModal: PropTypes.func.isRequired,
 }
